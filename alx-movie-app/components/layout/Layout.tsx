83695ad18733f74f16a84079c04f8af4d2da7b15
unknown <somegetiye@gmail.com>
2025-09-05 14:54:05 -0700

    update

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import { ComponentProps } from "@/interfaces";
 import Header from "./Header";
 import Footer from "./Footer";
 
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <>
       <Header />
-      <main className="flex-grow container mx-auto p-4">{children}</main>
+      <main className="min-h-screen">{children}</main>
       <Footer />
-    </div>
+    </>
   );
 };
 
--- alx-movie-app/pages/_app.tsx
+++ alx-movie-app/pages/_app.tsx
@@ -1,7 +1,6 @@
-// pages/_app.tsx
+import Layout from "@/components/layout/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "@/components/layouts/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
